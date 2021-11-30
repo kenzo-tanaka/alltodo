685c91b81c31250f20394ef4b538802e126d1e8c
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-30 11:38:36 +0900

    First commit

---
+++ lib/alltodo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "alltodo/version"
+
+module Alltodo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/alltodo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Alltodo
+  VERSION = "0.1.0"
+end
